5aafaeabb8665bb52c92497a6e34cc731728edd4
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-08-18 17:34:37 +0900

    add unlocked commands

--- first.ts
+++ first.ts
@@ -3,9 +3,18 @@ import { makefunc } from "bdsx/makefunc";
 import { CxxString, int32_t, void_t } from "bdsx/nativetype";
 import { procHacker } from "bdsx/prochacker";
 
-const force_allowed = new Set<string>(["allowlist"]);
-
 {
+    const force_unlocked = new Set<string>([
+        "allowlist",
+        "changesetting",
+        "codebuilder_actorinfo",
+        "wsserver",
+        "setmaxplayers",
+        "querytarget",
+        "listd",
+        "gettopsolidblock",
+    ]);
+
     const original = procHacker.hooking(
         "?registerCommand@CommandRegistry@@QEAAXAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@PEBDW4CommandPermissionLevel@@UCommandFlag@@3@Z",
         void_t,
@@ -17,7 +26,8 @@ const force_allowed = new Set<string>(["allowlist"]);
         int32_t,
         int32_t,
     )((self, name, desc, perm, flag1, flag2) => {
-        if (force_allowed.has(name)) {
+        console.log(name, CommandPermissionLevel[perm]);
+        if (force_unlocked.has(name)) {
             flag1 = 1; // glowing name
             perm = CommandPermissionLevel.Operator;
         }
