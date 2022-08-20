9e4080e8ac103f1a5276ee7771ecaa85bd109b2e
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-08-20 13:27:19 +0900

    first - loader

--- plugins/first/index.ts
+++ plugins/first/index.ts
@@ -1,13 +1,3 @@
 import { events } from "bdsx/event";
 
-events.serverOpen.on(() => {
-    console.log("[First] launching");
-});
-
-events.serverClose.on(() => {
-    console.log("[First] closed");
-});
-
-console.log("[First] Jobs started".blue);
 require("./src");
-console.log("[First] Jobs have done".blue);
--- plugins/first/src/index.ts
+++ plugins/first/src/index.ts
@@ -1,1 +1,11 @@
 import "./command";
+
+import * as fs from "fs";
+import * as path from "path";
+
+function load(filePath: string & fs.PathLike) {
+    require(filePath);
+    console.log(`[First] loaded - ${path.basename(filePath)}`.blue);
+}
+
+load("./command");
